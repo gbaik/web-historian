838ef73ae5854ee8ec7005ffbb505673adb04174
Hack Reactor Student <student@hackreactor.com>
2017-06-09 21:22:18 -0700

    Add pseudocode of approximation

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,30 +1,33 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('./http-helpers');
+
 // require more modules/folders here!
-var headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'application/json'
-};
 
 exports.handleRequest = function (req, res) {
   // If request method = post
-    // if request url in archive list (isUrlArchived)
-      // ***Somehow display the archived site***
+  // if (req.method === 'POST') {
+    // if request url in url list (isUrlInList)
+      // if request url in archive list (isUrlArchived)
+        // ***Somehow display the archived site***
+        // archive.paths.archivedSites + '/'
+      // else
+        // *** Somehow display the loading.html ***
     // else 
       // *** Somehow display the loading.html ***
       // add url to archive list (addUrlToList)
     // Set status code to 200
     // write response head
-    // write response end
-    
+    // write response end   
+  // }
+  
   // If request method = get
     // set status code to 200
     // write response head
     // write response end with the message
-  
+  // res.writeHead(200, httpHelper.headers);
+  res.writeHead(302, {Location: archive.paths.archivedSites + '/' + './public/index.html'}); 
+  res.end();
   // res.end(archive.paths.list);
 };
     
